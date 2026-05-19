aa9116ee52d30d85594c72dc7dd183af9f81ebdc
Shiv Yadav <shivtumca12@gmail.com>
2026-05-19 17:33:06 +0530

    Delete Cursor Project directory

--- Cursor Project/server/middleware/auth.js
+++
@@ -1,6 +0,0 @@
-function requireAdmin(req, res, next) {
-  if (req.session?.adminId) return next();
-  res.status(401).json({ error: "Unauthorized. Please log in." });
-}
-
-module.exports = { requireAdmin };
